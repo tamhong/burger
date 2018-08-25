ab907bdaf4975d9089058714ed4782b68bf82939
Tam Hong <tamhong.sd@gmail.com>
2018-08-24 18:40:45 -0700

    heroku connection

--- config/connection.js
+++ config/connection.js
@@ -8,6 +8,19 @@ var connection = mysql.createConnection({
     database: "burger_db"
 });
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "edQprs0<;D+l",
+        database: "burger_db"
+    });
+    
+};
+
 connection.connect(function(err) {
     if (err) {
         console.error("error connection: " + err.stack);
